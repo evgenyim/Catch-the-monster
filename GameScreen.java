6bb6e343b68fcf6a4c6c4364895279a739f21245
werty144 <anton-paramonov2000@yandex.ru>
2017-06-07 21:47:45 +0300

    Add files via upload

--- GameScreen.java
+++ GameScreen.java
@@ -7,17 +7,26 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.icu.util.TimeUnit;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+
+import static android.R.attr.delay;
+import static android.R.attr.y;
 
 /**
  * Created by user on 6/7/17.
  */
 
 public class GameScreen extends FrameLayout {
+
+    int x = 200;
 
     public GameScreen(@NonNull Context context) {
         super(context);
@@ -33,13 +42,16 @@ public class GameScreen extends FrameLayout {
 
     @Override
     protected void onDraw(Canvas canvas) {
+        Log.d("onDraw", "mi lubim java");
         Paint paint = new Paint();
         paint.setColor(Color.RED);
-        Paint paint2 = new Paint(Color.BLACK);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.table5);
-
-        canvas.drawCircle(pnt.Getx(),,50,paint);
-        canvas.drawBitmap(bitmap,100,500,paint2);
+        canvas.drawBitmap(bitmap,100,500,null);
+        canvas.drawCircle(x,x,50,paint);
+        while (x < 1000){
+            x ++;
+            invalidate();
+        }
 
     }
 
