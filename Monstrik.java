5ab4914b63d7d2c6468b050f6013440ef1a8055f
werty144 <anton-paramonov2000@yandex.ru>
2017-06-08 10:46:51 +0300

    Add files via upload

---
+++ Monstrik.java
@@ -0,0 +1,17 @@
+package com.example.user.catchthemonster;
+
+/**
+ * Created by User on 08.06.2017.
+ */
+
+public class Monstrik extends GameObject {
+    int x;
+    int y;
+
+
+    Monstrik(int x, int y){
+        super(x, y);
+    }
+
+
+}
