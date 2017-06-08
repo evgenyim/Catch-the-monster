9dcef062c197ecc8aee2525a8ad603f2437f62a7
werty144 <anton-paramonov2000@yandex.ru>
2017-06-08 10:45:38 +0300

    Add files via upload

--- GameScreen.java
+++ GameScreen.java
@@ -1,24 +1,17 @@
 package com.example.user.catchthemonster;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.icu.util.TimeUnit;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 
-import static android.R.attr.delay;
-import static android.R.attr.y;
+import java.util.LinkedList;
 
 /**
  * Created by user on 6/7/17.
@@ -26,8 +19,7 @@ import static android.R.attr.y;
 
 public class GameScreen extends FrameLayout {
 
-    int x = 200;
-
+    LinkedList<GameObject> gameObjects = new LinkedList<>();
     public GameScreen(@NonNull Context context) {
         super(context);
     }
@@ -44,15 +36,19 @@ public class GameScreen extends FrameLayout {
     protected void onDraw(Canvas canvas) {
         Log.d("onDraw", "mi lubim java");
         Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.table5);
-        canvas.drawBitmap(bitmap,100,500,null);
-        canvas.drawCircle(x,x,50,paint);
-        while (x < 1000){
-            x ++;
-            invalidate();
+        paint.setColor(Color.GREEN);
+
+        for (GameObject gameObject : gameObjects) {
+            canvas.drawCircle(gameObject.x, gameObject.y, 50, paint);
         }
 
+        postInvalidateDelayed(50);
     }
+
+    public void addGameObject(final GameObject gameObject) {
+        gameObjects.add(gameObject);
+        postInvalidateDelayed(50);
+    }
+
 
 }
