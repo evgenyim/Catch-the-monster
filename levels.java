ae5432f5d58520f8698a8c4fb08277609b1ce184
evgenyim <evgenyim6456@gmail.com>
2017-06-14 13:59:46 +0300

    Update levels.java

--- levels.java
+++ levels.java
@@ -1,20 +1,25 @@
 package com.example.user.catchthemonster;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class levels extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
+
+
 
         final Button btn1 =(Button) findViewById(R.id.button1);
         final levels this_ = this;
@@ -26,6 +31,23 @@ public class levels extends AppCompatActivity {
             }
         };
         btn1.setOnClickListener(listener1);
+        Log.d("3", String.valueOf(MainMenu.sPref.getBoolean("Level_1",true)));
+
+        final Button btn2 =(Button) findViewById(R.id.button2);
+        final View.OnClickListener listener2 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (MainMenu.sPref.getBoolean("Level_1", false)) {
+                    Log.d("2", "proshel");
+                    Intent intent = new Intent(this_, Level_2.class);
+                    startActivity(intent);
+                }
+
+            }
+        };
+        btn2.setOnClickListener(listener2);
+
+
 
     }
 
